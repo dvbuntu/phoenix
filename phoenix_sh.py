84de03a7eb1814fcb2459af0ba924fad6e7f8c8a
Danielle Van Boxel <the.dvb@gmail.com>
2024-02-18 22:20:27 -0700

    Clean up tail and sketch head

--- phoenix_sh.py
+++ phoenix_sh.py
@@ -1,4 +1,5 @@
 import shapely as sh
+import shapely.ops
 # conda activate tf # this works, I guess
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Circle, Polygon, Wedge
@@ -13,10 +14,10 @@ a = 1
 gap = a/4
 mid_x = 0
 mid_y = 5.5*a
-tip_x = -4*a
-tip_y = 4*mid_y/3
-c_x = 2*a # right polyline here
-c_y = 7*a/4
+tip_x = -3.6*a
+tip_y = 5*mid_y/4
+c_x = 2.15*a # right polyline here
+c_y = 10*a/4
 phi = np.pi/6
 alph = np.pi/3
 sig = np.pi/18
@@ -71,6 +72,10 @@ b12 = next_line(b11, blue_bot, wing_tip, a)
 b12_ext = Line.from_points(b12.coords[2], b12.coords[1])
 b11_piece = Line.from_points(b11.coords[0], b11.coords[1])
 b1_int = np.round(b12_ext.intersect_line(b11_piece),4)
+## redo the lines to hit at this intersection
+blue_bot = Line.from_points([-10*a,b1_int[1]], [10*a,b1_int[1]])
+b11 = next_line(p12, blue_bot, wing_tip, gap)
+b12 = next_line(b11, blue_bot, wing_tip, a)
 
 B1 = Polygon(b11.coords[:] + list(reversed(b12.coords[1:])) + [tuple(b1_int)],
         alpha=1.,
@@ -80,11 +85,48 @@ B1 = Polygon(b11.coords[:] + list(reversed(b12.coords[1:])) + [tuple(b1_int)],
 
 patches.append(B1)
 
-# head
-head1 = next_line(w1, bottom, wing_tip, -gap)
-head_base = head1.coords[5:7]
-#head_base = sh.LineString(
 
+
+# head
+#head_bot = Line.from_points([-10*a,c_y+a/4], [10*a,c_y+a/4])
+#head_turn = (head_bot.point[1] + mid_y)/2
+#head_top_cutoff = Line.from_points([-10*a,head_turn], [10*a,head_turn])
+w1_piece = sh.LineString(w1.coords[2:4])
+h1 = w1_piece.offset_curve(-gap, join_style=2, mitre_limit=gap)
+h1s = sh.ops.substring(h1, 0, 0.5, normalized=True)
+
+h1sl = sh.LineString([h1s.coords[1], [h1s.coords[1][0]-gap/2, h1s.coords[1][1]]])
+h1sd = sh.LineString([h1s.coords[0], [h1s.coords[0][0], h1s.coords[0][1]-gap/2]])
+
+## get normal direction
+d = Line.from_points(*w1_piece.coords[:]).direction
+d = np.array([-d[1],d[0]])
+
+h1sn1 = Line(h1sl.coords[1], d/np.sqrt(np.sum(d**2)))
+h1sn1 = sh.LineString([h1sn1.point, h1sn1.to_point(-a)])
+
+h1sn2 = Line(h1sd.coords[1], d/np.sqrt(np.sum(d**2)))
+h1sn2 = sh.LineString([h1sn2.point, h1sn2.to_point(-a/2)])
+
+h1st = sh.LineString([h1sn1.coords[1], [h1sn2.coords[1][0], h1sn1.coords[1][1]]])
+h1sr = sh.LineString([h1sn2.coords[1], [h1sn2.coords[1][0], h1sn1.coords[1][1]]])
+
+h1s = h1s.union(h1sl)
+h1s = h1s.union(h1sn1)
+h1s = h1s.union(h1sd)
+h1s = h1s.union(h1st)
+h1s = h1s.union(h1sr)
+h1s = h1s.union(h1sn2)
+h1s = sh.ops.linemerge(h1s.normalize())
+
+P2 = Polygon(h1s.coords[:],
+        alpha=1.,
+        color='#F5A9B8', # pink
+        fill=True
+        )
+patches.append(P2)
+
+# plot the patches
 p = PatchCollection(patches, match_original=True)
 fig, ax  = plt.subplots()
 ax.add_collection(p)
@@ -93,5 +135,6 @@ ax.set_ylim(0,12*a)
 plt.gca().set_aspect('equal')
 
 
-sh.plotting.plot_line(head1)
-
+#sh.plotting.plot_line(h1)
+#sh.plotting.plot_line(h1s,color='red')
+plt.savefig('latest.png')
